4d97141da86856312923a5e3f44b3359e0092dc8
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-08 08:23:04 +0600

    Scaffolding and working with asset and pubspce file

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+
 //the main function of all flutter code
 void main() {
   runApp(
-      MaterialApp(
-        home: Center(
-          child: Text("Hello World"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title:Center(
+            child: Text("I AM RICH"),
+          ), 
         ),
-        debugShowCheckedModeBanner: false,
+        body: Center(
+          child:Image(
+            image:AssetImage('images/diamond.png'),
+          )
       ),
-    );
+    ),
+     debugShowCheckedModeBanner: false,
+    )
+  );
 }

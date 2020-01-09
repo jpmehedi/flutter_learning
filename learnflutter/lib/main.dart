cb37bb0dae15e9fa510f0ac5a8ac349b7c34e997
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-09 18:49:19 +0600

    learn How to use container widget and verious container property . Remember container is a single child widget

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -13,7 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Home"),),
-        body: Center(child: Text("Hello Flutter"),),
+        body:SafeArea(
+            child: Container(
+            height: 100.00,
+            width: 100.00,
+            color: Colors.brown[200],
+            //margin: EdgeInsets.all(50),
+            margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            //margin: EdgeInsets.only(left:50),
+           //margin: EdgeInsets.symmetric(vertical: 50,horizontal: 100),
+
+           //padding: EdgeInsets.all(25),
+            padding: EdgeInsets.fromLTRB(10, 50, 10, 8),
+            child: Text("Hello Flutter"),
+          ),
+        )
       ),
     );
   }

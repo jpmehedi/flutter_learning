40f7189bf63b7d1938d1a6dae6af41f4b7a59371
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-09 18:17:23 +0600

    learn hot reload and hot restart

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -2,26 +2,19 @@ import 'package:flutter/material.dart';
 
 
 //the main function of all flutter code
-void main() {
+void main(){
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title:Center(
-            child: Text("I AM RICH"),
-          ), 
-        ),
-        body: Center(
-          child:Image(
-            //network image dose not working i can not findout this 
-            //image:NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
-            image:AssetImage('images/diamond.png'),
-          )
-      ),
-    ),
-     debugShowCheckedModeBanner: false,
-    )
+    MyApp()
   );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Home"),),
+        body: Center(child: Text("Hello Flutter"),),
+      ),
+    );
+  }
 }

96647f2d2ead3a1739b3989a69a1c5c6cf54fc66
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-11 20:01:15 +0600

    Adding Material Icons with the Icon Widget

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -35,10 +35,60 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
-                  
                 ),
-                
               ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.00),
+                margin:
+                    EdgeInsets.symmetric(horizontal: 20.00, vertical: 20.00),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.00,
+                    ),
+                    Text(
+                      '+880-179080825',
+                      style: TextStyle(
+                        fontFamily: 'SourceSnasPro',
+                        color: Colors.teal[200],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.00,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.00),
+                margin: EdgeInsets.symmetric(horizontal: 20.00,vertical: 20.00),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.00,
+                    ),
+                    Text(
+                      'mehedihasan@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.00,
+                        color: Colors.teal[300],
+
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),

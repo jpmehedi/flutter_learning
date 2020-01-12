553a4d980e013c46ac0f3e068f58e5c4eb91fd9b
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-12 10:32:12 +0600

    Using the Expanded Widget to Create Flexible Layouts

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -2,89 +2,37 @@ import 'package:flutter/material.dart';
 
 //the main function of all flutter code
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text('Dicee'),
+        ),
+        body: DiceePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class DiceePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/Avatar.png'),
-              ),
-              Text(
-                "Mr. Mehedi",
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.00,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Flutter Develper'.toUpperCase(),
-                style: TextStyle(
-                  fontFamily: 'SourceSnasPro',
-                  fontSize: 20.00,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 10.00,
-                width: 150.00,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                margin:
-                    EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.00),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+880- 1790180825',
-                    style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.00,
-                        color: Colors.teal[200]),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.00, horizontal: 25.0),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      'mehedihasan@gmail.com',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.00,
-                        color: Colors.teal[300],
-                      ),
-                    )),
-              ),
-            ],
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image(
+            //width: 200.00,
+            image: AssetImage('images/dice1.png'),
           ),
         ),
-      ),
-      debugShowCheckedModeBanner: false,
+        Expanded(
+          child: Image(
+            //width: 200.00,
+            image: AssetImage('images/dice2.png'),
+          ),
+        ),
+      ],
     );
   }
 }

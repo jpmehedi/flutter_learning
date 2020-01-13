e86d48bc61b53c74c7cb52d03807d5470d25e67e
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-13 10:09:14 +0600

    What are Flutter & Dart Packages and how to ready flutter  pakagse for use

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -1,55 +1,16 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:english_words/english_words.dart';
 
-//the main function of all flutter code
-void main() {
+void main(){
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
+        body: SafeArea(
+          child: Center(child: Text(adjectives.last),),
         ),
-        body: DiceePage(),
       ),
     ),
   );
 }
 
-class DiceePage extends StatefulWidget {
-  _DiceePageState createState() => _DiceePageState();
-}
 
-class _DiceePageState extends State<DiceePage> {
-  int leftDiceNumber = 5;
-  int rightDiceNumber = 3;
-
-  void changeDice() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {changeDice();},
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {changeDice();},
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
